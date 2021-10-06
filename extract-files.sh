eb6a826aa86d527eee68e9158f7f21d15b37e9eb
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-10-06 15:25:27 +0700

    X00TD: Update extract-file.sh

--- extract-files.sh
+++ extract-files.sh
@@ -6,17 +6,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-function blob_fixup() {
-    case "${1}" in
-        vendor/lib/hw/camera.sdm660.so)
-            "${PATCHELF}" --add-needed "libcamera_sdm660_shim.so" "${2}"
-            ;;
-        vendor/lib64/libgf_ca.so)
-            sed -i 's|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g' "${2}"
-            ;;
-    esac
-}
-
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
@@ -25,8 +14,8 @@ fi
 
 set -e
 
-export DEVICE=whyred
+export DEVICE=X00TD
 export DEVICE_COMMON=sdm660-common
-export VENDOR=xiaomi
+export VENDOR=asus
 
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
