31622c47dc09e91c337578a18540c4f91fd0084f
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-10-06 11:22:04 +0700

    X00TD: Use keymaster prebuilt

--- device.mk
+++ device.mk
@@ -31,11 +31,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
 
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
